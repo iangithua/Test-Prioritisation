7f4b38526f2e1cd7e75f7b304f9a88a48b7e27ec
Ian Githua <40959077+iangithua@users.noreply.github.com>
2025-11-25 17:18:22 +0100

    dev: Test Order Generator const

--- src/de/uni_passau/fim/se2/se/test_prioritisation/encodings/TestOrderGenerator.java
+++ src/de/uni_passau/fim/se2/se/test_prioritisation/encodings/TestOrderGenerator.java
@@ -2,6 +2,7 @@ package de.uni_passau.fim.se2.se.test_prioritisation.encodings;
 
 import de.uni_passau.fim.se2.se.test_prioritisation.mutations.Mutation;
 
+import java.util.Objects;
 import java.util.Random;
 
 /**
@@ -18,8 +19,16 @@ public class TestOrderGenerator implements EncodingGenerator<TestOrder> {
      * @param mutation   the elementary transformation that the generated orderings will use
      * @param testCases  the number of test cases in the ordering
      */
+
+    private final Random random;
+    private final Mutation<TestOrder> mutation;
+    private final int testCases;
+
+
     public TestOrderGenerator(final Random random, final Mutation<TestOrder> mutation, final int testCases) {
-        throw new UnsupportedOperationException("Implement me");
+        this.random = Objects.requireNonNull(random);
+        this.mutation = Objects.requireNonNull(mutation);
+        this.testCases = testCases;
     }
 
     /**
@@ -29,6 +38,21 @@ public class TestOrderGenerator implements EncodingGenerator<TestOrder> {
      */
     @Override
     public TestOrder get() {
-        throw new UnsupportedOperationException("Implement me");
+        // Build array [0, 1, 2, ..., testCases - 1]
+        int[] positions = new int[testCases];
+        for (int i = 0; i < testCases; i++) {
+            positions[i] = i;
+        }
+
+        // Fisher–Yates shuffle
+        for (int i = testCases - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            int temp = positions[i];
+            positions[i] = positions[j];
+            positions[j] = temp;
+        }
+
+        // Create a new TestOrder using mutation and shuffled array
+        return new TestOrder(mutation, positions);
     }
 }
