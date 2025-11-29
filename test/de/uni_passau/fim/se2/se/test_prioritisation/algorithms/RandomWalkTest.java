696470d408f098799def44867f74af9001186fd4
Ian Githua <40959077+iangithua@users.noreply.github.com>
2025-11-29 14:22:40 +0100

    fix: test cases errors

--- test/de/uni_passau/fim/se2/se/test_prioritisation/algorithms/RandomWalkTest.java
+++ test/de/uni_passau/fim/se2/se/test_prioritisation/algorithms/RandomWalkTest.java
@@ -1,15 +1,5 @@
 package de.uni_passau.fim.se2.se.test_prioritisation.algorithms;
 
-import de.uni_passau.fim.se2.se.test_prioritisation.encodings.Encoding;
-import de.uni_passau.fim.se2.se.test_prioritisation.encodings.EncodingGenerator;
-import de.uni_passau.fim.se2.se.test_prioritisation.fitness_functions.FitnessFunction;
-import de.uni_passau.fim.se2.se.test_prioritisation.mutations.Mutation;
-import de.uni_passau.fim.se2.se.test_prioritisation.stopping_conditions.StoppingCondition;
-import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.atomic.AtomicInteger;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class RandomWalkTest {
 //Test
